df4dd943cbcc6fe9739993a645dcc451e8d2707c
Tech <Tech@DESKTOP-V6HVK9J>
2019-09-18 16:49:26 -0500

    Interactive Map

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -70,10 +70,10 @@ public class HelloWorld extends PApplet
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
-		
+	    map2.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
-		
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
 
