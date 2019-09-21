2389d241b4475f2542b977a3c2be7cd04797eed1
Tech <Tech@DESKTOP-V6HVK9J>
2019-09-21 12:55:24 -0500

    Changing Map Provider

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -1,5 +1,5 @@
 package module1;
-
+import de.fhpotsdam.unfolding.providers.*;
 import processing.core.PApplet;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
@@ -27,7 +27,7 @@ public class HelloWorld extends PApplet
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	// IF YOU ARE WORKING OFFLINE: Change the value of this variable to true
-	private static final boolean offline = true;
+	private static final boolean offline = false;
 	
 	/** The map we use to display our home town: La Jolla, CA */
 	UnfoldingMap map1;
@@ -46,17 +46,17 @@ public class HelloWorld extends PApplet
 		this.background(200, 200, 200);
 		
 		// Select a map provider
-		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+		UnfoldingMap provider = new UnfoldingMap(this, new Microsoft.AerialProvider());
 		// Set a zoom level
 		int zoomLevel = 10;
 		
-		if (offline) {
-			// If you are working offline, you need to use this provider 
-			// to work with the maps that are local on your computer.  
-			provider = new MBTilesMapProvider(mbTilesString);
-			// 3 is the maximum zoom level for working offline
-			zoomLevel = 3;
-		}
+//		if (offline) {
+//			// If you are working offline, you need to use this provider 
+//			// to work with the maps that are local on your computer.  
+//			provider = new MBTilesMapProvider(mbTilesString);
+//			// 3 is the maximum zoom level for working offline
+//			zoomLevel = 3;
+//		}
 		
 		// Create a new UnfoldingMap to be displayed in this window.  
 		// The 2nd-5th arguments give the map's x, y, width and height
@@ -65,15 +65,15 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
-		map2 = new UnfoldingMap(this, 420, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 50, 50, 350, 500);
+		map2 = new UnfoldingMap(this, 420, 50, 350, 500);
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
 	    map2.zoomAndPanTo(zoomLevel, new Location(41.3f, -92.6f));
 		// This line makes the map interactive
-		MapUtils.createDefaultEventDispatcher(this, map1);
-		MapUtils.createDefaultEventDispatcher(this, map2);
+		MapUtils.createDefaultEventDispatcher(this, map1, map2);
+//		MapUtils.createDefaultEventDispatcher(this, map2);
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
 
